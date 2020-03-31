d5f08e1b00e77da5da4b2058650f1c92ef8b5089
Maner <antonin.huaut+github@gmail.com>
2020-03-31 19:44:47 +0200

    fix

--- src/components/ListBF.vue
+++ src/components/ListBF.vue
@@ -209,13 +209,12 @@ export default {
   },
   mounted() {
     fetch(
-      `/static/assets/data/${this.type}.json`
+      `https://raw.githubusercontent.com/Cat333Pokemon/critterpedia/master/db/${this.type}.json`
     )
       .then(res => res.json())
       .then(res => {
-        console.log(res);
         this.mainTab.dataList = res.map(item => {
-          item.img = require(`../assets/${this.type}/${item.name}.png`);
+          item.img = `https://raw.githubusercontent.com/Cat333Pokemon/critterpedia/master/images/${this.type}/${item.name}.png`;
           item.price = item.price == -1 ? "N/A" : item.price;
           return item;
         });
